f6e8244cfd6990d8669c0c0bb4963ba4abc5e405
user <user@192.168.8.100>
2018-09-12 12:51:30 +0700

    Initial Commit

---
+++ src/Latihan/HitungFibonacci.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author user
+ */
+public class HitungFibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void tampilJudul(String identitas) {
+        // TODO code application logic here
+        System.out.println("identitas : " + identitas);
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    public static void main(String[] args){
+        String identitas = "Najwa Kusuma Mustafa / X RPL 5 / 28";
+        tampilJudul(identitas);
+    }
+}
